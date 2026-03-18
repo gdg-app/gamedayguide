801a76bd5859c67b061e0481aa3bb50f5ab68ebd
gdg-app <thrower210@gmail.com>
2026-03-18 14:09:30 -0400

    Update field-suggestions.js

--- api/field-suggestions.js
+++ api/field-suggestions.js
@@ -123,7 +123,7 @@ export default async function handler(req, res) {
       return res.status(200).json({ ok: true, data: [] });
     }
 
-    const apiKey = "BAD_KEY_TEST";;
+   const apiKey = process.env.MAPS_API_KEY;
 
     const url =
       "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=" +
